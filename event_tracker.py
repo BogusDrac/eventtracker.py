f1ebe0cbe4621c6b48329cf201d12a4c929e065b
Bogusdrac <bogusdracula7@gmail.com>
2025-10-31 11:28:00 +0200

    created enviroment for adding plyer library for notification and added requirements file for installing required libraries and even_tracker.py as main file

---
+++ event_tracker.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+from datetime import datetime
+import json
+import os
+from plyer import notification
+
+DATA_FILE = "events.json"
